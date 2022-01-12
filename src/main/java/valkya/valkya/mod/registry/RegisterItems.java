f91e0a4a8fda3438548d8a6d724becc6efd7957c
Osmiooo <owencraft2000@gmail.com>
2022-01-12 19:06:41 +0100

    Lingots de Cesium et de Zirconium

--- src/main/java/valkya/valkya/mod/registry/RegisterItems.java
+++ src/main/java/valkya/valkya/mod/registry/RegisterItems.java
@@ -1,8 +1,21 @@
 package valkya.valkya.mod.registry;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import valkya.valkya.mod.ValkyaMod;
+
 public class RegisterItems {
 
+
+    public static final Item CESIUM_INGOT = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item ZIRCONIUM_INGOT = new Item(new Item.Settings().group(ItemGroup.MISC));
+
     public static void RegisterItems() {
+
+        Registry.register(Registry.ITEM, new Identifier(ValkyaMod.MOD_ID, "cesium_ingot"), CESIUM_INGOT);
+        Registry.register(Registry.ITEM, new Identifier(ValkyaMod.MOD_ID, "zirconium_ingot"), ZIRCONIUM_INGOT);
 
     }
 }
