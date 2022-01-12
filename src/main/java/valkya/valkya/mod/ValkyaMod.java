afda74550ad3147303c73bdb8dd1942e3faac1fe
Osmiooo <owencraft2000@gmail.com>
2022-01-12 17:48:57 +0100

    Initial commit

---
+++ src/main/java/valkya/valkya/mod/ValkyaMod.java
@@ -0,0 +1,13 @@
+package valkya.valkya.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ValkyaMod implements ModInitializer {
+
+    public static final String MOD_ID = "valkya";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
