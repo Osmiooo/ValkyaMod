00c740d5e4cf3b9ea4829ed8aa0a295b51643777
Osmiooo <owencraft2000@gmail.com>
2022-01-12 18:18:02 +0100

    Bloc de Cesium

--- src/main/java/valkya/valkya/mod/registry/RegisterBlocks.java
+++ src/main/java/valkya/valkya/mod/registry/RegisterBlocks.java
@@ -1,8 +1,24 @@
 package valkya.valkya.mod.registry;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import valkya.valkya.mod.ValkyaMod;
+
 public class RegisterBlocks {
 
+
+    public static final Block CESIUM_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(5f, 6f));
+
     public static void RegisterBlocks() {
+
+        Registry.register(Registry.BLOCK, new Identifier(ValkyaMod.MOD_ID, "cesium_block"), CESIUM_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(ValkyaMod.MOD_ID, "cesium_block"), new BlockItem(CESIUM_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
     }
 }
