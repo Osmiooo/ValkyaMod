cf2fbaf77d29f4251792794badbeccea5e6bd8b4
Osmiooo <owencraft2000@gmail.com>
2022-01-12 18:36:04 +0100

    Bloc de Zirconium

--- src/main/java/valkya/valkya/mod/registry/RegisterBlocks.java
+++ src/main/java/valkya/valkya/mod/registry/RegisterBlocks.java
@@ -10,16 +10,20 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import valkya.valkya.mod.ValkyaMod;
 import valkya.valkya.mod.blocks.CesiumBlock;
+import valkya.valkya.mod.blocks.ZirconiumBlock;
 
 public class RegisterBlocks {
 
 
     public static final Block CESIUM_BLOCK = new CesiumBlock(FabricBlockSettings.of(Material.METAL).requiresTool().strength(5f, 6f));
+    public static final Block ZIRCONIUM_BLOCK = new ZirconiumBlock(FabricBlockSettings.of(Material.METAL).requiresTool().strength(5f, 6f));
 
     public static void RegisterBlocks() {
 
         Registry.register(Registry.BLOCK, new Identifier(ValkyaMod.MOD_ID, "cesium_block"), CESIUM_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(ValkyaMod.MOD_ID, "cesium_block"), new BlockItem(CESIUM_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.BLOCK, new Identifier(ValkyaMod.MOD_ID, "zirconium_block"), ZIRCONIUM_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(ValkyaMod.MOD_ID, "zirconium_block"), new BlockItem(ZIRCONIUM_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
     }
 }
